c3b395fbf09cbbfa7f84279b000b975a68ff8a57
EfaPower <efagodspower042@gmail.com>
2025-07-14 16:04:10 +0100

    Update views.py

--- myapp/views.py
+++ myapp/views.py
@@ -4,6 +4,6 @@ from django.http import HttpResponse
 from django.template import loader
 
 # Create your views here.
-def main(request):
-    template = loader.get_template('main.html')
+def home(request):
+    template = loader.get_template('home.html')
     return HttpResponse(template.render())
